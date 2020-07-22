d993fa0464fcf31176c51904e07e01205c1c92a9
Lokendra <lokendrakumar16@gmail.com>
2020-07-22 18:58:53 +0530

    Added code
    Added code

---
+++ Auth.Demo/Controllers/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Auth.Demo.Controllers
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Auth.Demo/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Auth.Demo.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IJwtAuthenticationManager _jwtAuthenticationManager;
+        public UserController(IJwtAuthenticationManager jwtAuthenticationManager)
+        {
+            this._jwtAuthenticationManager = jwtAuthenticationManager;
+        }
+        // GET: api/<UserController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] User user)
+        {
+            var token = _jwtAuthenticationManager.Authenticate(user.UserName, user.Password);
+            if (token == null)
+            {
+                return Unauthorized();
+            }
+            return Ok(token);
+        }
+    }
+}
